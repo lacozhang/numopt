b1b1c7538d06c542e0ea9f2bf17c8be870a5d827
yu zhang <lacozhang@gmail.com>
2017-05-23 20:23:05 +0800

    enable namespace for nn model

--- nn/nnsequencedata.cpp
+++ nn/nnsequencedata.cpp
@@ -1,2 +1,16 @@
 #include "nnsequencedata.h"
 
+namespace NNModel {
+
+	void NNSequenceFeature::AppendSequenceFeature(boost::shared_ptr<SentenceFeature>& feat){
+		features_.push_back(feat);
+	}
+
+	void NNSequenceFeature::SetSequenceFeature(boost::shared_ptr<SentenceFeature>& feat, int index){
+		if (features_.size() < index + 1){
+			BOOST_LOG_TRIVIAL(info) << "increase the size of features";
+			features_.resize(index + 1);
+		}
+	}
+}
+
--- nn/nnsequencedata.h
+++ nn/nnsequencedata.h
@@ -4,117 +4,127 @@
 #define __NNSEQUENCE_DATA_H__
 #include <vector>
 #include <boost/shared_ptr.hpp>
+#include <boost/log/trivial.hpp>
 #include "../typedef.h"
 
-class NNSequenceFeature;
+namespace NNModel {
 
-class SentenceFeature {
-public:
-	SentenceFeature() {
-	}
+	class NNSequenceFeature;
 
-	~SentenceFeature() {
-
-	}
-
-	DataSamples& SparseBinaryFeature() {
-		return sparsebinary_;
-	}
-
-	DataSamples::ConstRowXpr SparseBinaryFeature(int pos) const {
-		return sparsebinary_.row(pos);
-	}
-
-	DataSamples& SparseFeature() {
-		return sparse_;
-	}
-
-	DataSamples::ConstRowXpr SparseFeature(int pos) const {
-		return sparse_.row(pos);
-	}
-
-	DenseMatrix& DenseFeature() {
-		return dense_;
-	}
-
-	DenseMatrix::ConstRowXpr DenseFeature(int pos) const {
-		return dense_.row(pos);
-	}
-
-private:
-	DataSamples sparsebinary_;
-	DataSamples sparse_;
-	DenseMatrix dense_;
-};
-
-class SentenceLabel {
-public:
-
-	const static int UNKNOWN = -1;
-
-	SentenceLabel() {
-		labels_.resize(0);
-	}
-
-	explicit SentenceLabel(int size) {
-		labels_.resize(size);
-		for (int i = 0; i < labels_.size(); ++i) {
-			labels_.coeffRef(i) = SentenceLabel::UNKNOWN;
+	class SentenceFeature {
+	public:
+		SentenceFeature() {
 		}
-	}
 
-	~SentenceLabel() {
-	}
+		~SentenceFeature() {
 
-	int GetLabel(int idx) {
-		return labels_[idx];
-	}
+		}
 
-	void SetLabel(int idx, int label) {
-		labels_[idx] = label;
-	}
+		DataSamples& SparseBinaryFeature() {
+			return sparsebinary_;
+		}
 
-	LabelVector& GetLabels() {
-		return labels_;
-	}
+		DataSamples::ConstRowXpr SparseBinaryFeature(int pos) const {
+			return sparsebinary_.row(pos);
+		}
 
-	void SetLabels(std::vector<int>& labels) {
-		labels_.resize(labels.size());
-		for (int i = 0; i < labels.size(); ++i)
-			labels_.coeffRef(i) = labels[i];
-	}
+		DataSamples& SparseFeature() {
+			return sparse_;
+		}
 
-	void SetLabels(LabelVector& labels) {
-		labels_ = labels;
-	}
+		DataSamples::ConstRowXpr SparseFeature(int pos) const {
+			return sparse_.row(pos);
+		}
 
-private:
-	LabelVector labels_;
-};
+		DenseMatrix& DenseFeature() {
+			return dense_;
+		}
 
-class NNSequenceFeature {
-public:
-	NNSequenceFeature();
-	~NNSequenceFeature();
-	
-	void AppendSequenceFeature(boost::shared_ptr<SentenceFeature>& feat);
-	void SetSequenceFeature(boost::shared_ptr<SentenceFeature>& feat);
-	SentenceFeature& GetSequenceFeature(int index);
-	size_t NumSamples();
+		DenseMatrix::ConstRowXpr DenseFeature(int pos) const {
+			return dense_.row(pos);
+		}
 
-private:
-	std::vector<boost::shared_ptr<SentenceFeature>> features_;
-};
+	private:
+		DataSamples sparsebinary_;
+		DataSamples sparse_;
+		DenseMatrix dense_;
+	};
 
-class NNSequenceLabel {
-public:
-	NNSequenceLabel();
-	~NNSequenceLabel();
+	class SentenceLabel {
+	public:
 
+		const static int UNKNOWN = -1;
 
+		SentenceLabel() {
+			labels_.resize(0);
+		}
 
-private:
-	std::vector<boost::shared_ptr<SentenceLabel>> labels_;
-};
+		explicit SentenceLabel(int size) {
+			labels_.resize(size);
+			for (int i = 0; i < labels_.size(); ++i) {
+				labels_.coeffRef(i) = SentenceLabel::UNKNOWN;
+			}
+		}
+
+		~SentenceLabel() {
+		}
+
+		int GetLabel(int idx) {
+			return labels_[idx];
+		}
+
+		void SetLabel(int idx, int label) {
+			labels_[idx] = label;
+		}
+
+		LabelVector& GetLabels() {
+			return labels_;
+		}
+
+		void SetLabels(std::vector<int>& labels) {
+			labels_.resize(labels.size());
+			for (int i = 0; i < labels.size(); ++i)
+				labels_.coeffRef(i) = labels[i];
+		}
+
+		void SetLabels(LabelVector& labels) {
+			labels_ = labels;
+		}
+
+	private:
+		LabelVector labels_;
+	};
+
+	class NNSequenceFeature {
+	public:
+		NNSequenceFeature();
+		~NNSequenceFeature();
+
+		void AppendSequenceFeature(boost::shared_ptr<SentenceFeature>& feat);
+		void SetSequenceFeature(boost::shared_ptr<SentenceFeature>& feat, int idx);
+		SentenceFeature& GetSequenceFeature(int index);
+		SentenceFeature& operator[](int index);
+		size_t NumSamples();
+
+	private:
+		std::vector<boost::shared_ptr<SentenceFeature>> features_;
+	};
+
+	class NNSequenceLabel {
+	public:
+		NNSequenceLabel();
+		~NNSequenceLabel();
+
+		void AppendSequenceLabel(boost::shared_ptr<SentenceLabel>& label);
+		void SetSequenceLabel(boost::shared_ptr<SentenceLabel>& label, int idx);
+		SentenceLabel& GetSequenceLabel(int index);
+		SentenceLabel& operator[](int index);
+		size_t NumSamples();
+
+	private:
+		std::vector<boost::shared_ptr<SentenceLabel>> labels_;
+	};
+
+}
 
 #endif // !__NNSEQUENCE_DATA_H__
