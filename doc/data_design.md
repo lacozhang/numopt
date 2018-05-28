92a6df85a22f04d4c2e1c86c2e1dab1eb3cad8d4
Edwin Zhang <edwinzhang@fb.com>
2018-05-28 20:04:42 +0100

    code structure re-organize

---
+++ doc/data_design.md
@@ -0,0 +1,4 @@
+General idea about how the data should be represented in this tool (most of the design comes from )
+1. data will be read in protocol buffer
+2. everything will be presented by matrix (eigen3 will be the interface design)
+3. there will be 2 different ways to read the data. (read in column/read in row)
---
+++ src/test/text_data.cpp
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  text_data.cpp
+ *
+ *    Description:  unit test for test data reading
+ *
+ *        Version:  1.0
+ *        Created:  05/28/2018 19:54:10
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  lacozhang (), lacozhang@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#define BOOST_TEST_MODULE "TextParser"
+#include "data/text_parser.h"
+#include <iostream>
+#include <string>
+
+BOOST_AUTO_TEST_CASE(ReadLibSVM) {
+  std::string line = "0 20:3 39:2.3";
+  std::cout << "Test";
+}
