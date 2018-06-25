aacdf1a97848f4a9a038660a05e424978b51df58
Edwin Zhang <edwinzhang@fb.com>
2018-06-25 20:47:27 +0100

    dense matrix

---
+++ src/util/include/util/dense_matrix.h
@@ -0,0 +1,18 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  dense_matrix.h
+ *
+ *    Description:  inherit the matrix interface
+ *
+ *        Version:  1.0
+ *        Created:  06/25/2018 20:45:54
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  lacozhang (), lacozhang@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
