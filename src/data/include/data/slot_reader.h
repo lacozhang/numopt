343dd88d0503f4ca94b5a8414788dfbf11eb9a95
lacozhang <lacozhang@gmail.com>
2018-07-10 08:38:54 +0100

    add data support for some feature group

--- src/data/include/data/slot_reader.h
+++ src/data/include/data/slot_reader.h
@@ -17,3 +17,68 @@
  */
 
 #pragma once
+#include "data/common.h"
+#include "proto/example.pb.h"
+#include "util/dynamic_array_impl.h"
+
+namespace mltools {
+class SlotReader {
+public:
+  SlotReader() {}
+  SlotReader(const DataConfig &data, const DataConfig &cache) {
+    init(data, cache);
+  }
+
+  void init(const DataConfig &data, const DataConfig &cache);
+
+  int read(ExampleInfo *info = nullptr);
+
+  template <typename V> MatrixInfo info(int slotId) const {
+    return readMatrixInfo(info_, slotId, sizeof(uint64), sizeof(V));
+  }
+
+  DArray<size_t> offset(int slotId);
+  DArray<uint64> index(int slotId);
+  template <typename V> DArray<V> value(int slotId) const;
+
+  void clear(int slotId) {
+    offsetCache_.erase(slotId);
+    indexCache_.erase(slotId);
+  }
+
+private:
+  std::string cacheName(const DataConfig &data, int slotId) const;
+  size_t nnzEle(int slotId) const;
+  bool readOneFile(const DataConfig &data, int ithFile);
+
+  std::string cache_;
+  DataConfig data_;
+  ExampleInfo info_;
+  std::mutex mu_;
+  size_t loadedFileCount_;
+  std::vector<uint32> numEx_;
+  std::unordered_map<int, DArray<size_t>> offsetCache_;
+  std::unordered_map<int, DArray<uint64>> indexCache_;
+  std::unordered_map<int, SlotInfo> slotsInfo_;
+};
+
+template <typename V> DArray<V> SlotReader::value(int slotId) const {
+  DArray<V> val;
+  if (nnzEle(slotId) == 0) {
+    return val;
+  }
+  for (int i = 0; i < data_.file_size(); ++i) {
+    std::string cacheFilepath = cacheName(ithFile(data_, i), slotId) + ".value";
+    DArray<V> raw;
+    CHECK(raw.readFromFile(cacheFilepath));
+    DArray<V> full;
+    full.uncompressFrom(raw);
+    auto prevSize = val.size();
+    int currSize = prevSize + full.size();
+    val.resize(currSize);
+    std::memcpy(val.data() + prevSize, full.data(), sizeof(V) * full.size());
+  }
+  ASSERT_EQ(val.size(), nnzEle(slotId)) << " Read corrupted data";
+  return val;
+}
+} // namespace mltools
--- src/data/src/slot_reader.cpp
+++ src/data/src/slot_reader.cpp
@@ -17,3 +17,32 @@
  */
 
 #include "data/slot_reader.h"
+#include "data/info_parser.h"
+#include "data/text_parser.h"
+#include "util/recordio.h"
+#include "util/threadpool.h"
+
+namespace mltools {
+
+void SlotReader::init(const DataConfig &data, const DataConfig &cache) {
+  ASSERT_TRUE(cache.file_size() == 1);
+  data_ = data;
+  cache_ = cache.file(0);
+}
+
+std::string SlotReader::cacheName(const DataConfig &data, int slotId) const {
+  CHECK_GE(data.file_size(), 1);
+  return cache_ + data.file(0) + "_slot_" + std::to_string(slotId);
+}
+
+size_t SlotReader::nnzEle(int slotId) const {
+  size_t nnz = 0;
+  for (int i = 0; i < info_.slot_size(); ++i) {
+    if (info_.slot(i).id() == slotId) {
+      nnz = info_.slot(i).nnz_ele();
+    }
+  }
+
+  return nnz;
+}
+} // namespace mltools
