d9d2b1c7c978152316d7a2e9f95d712f1c91f360
lacozhang <lacozhang@gmail.com>
2018-07-23 08:49:19 +0100

    add missing Filter

--- src/filter/src/filter.cpp
+++ src/filter/src/filter.cpp
@@ -11,7 +11,7 @@
  *       Compiler:  gcc
  *
  *         Author:  lacozhang (), lacozhang@gmail.com
- *   Organization:  
+ *   Organization:
  *
  * =====================================================================================
  */
@@ -23,3 +23,32 @@
 #include "filter/frequency_filter.h"
 #include "filter/key_caching.h"
 #include "filter/sparse_filter.h"
+
+namespace mltools {
+Filter *Filter::create(const mltools::FilterConfig &conf) {
+  switch (conf.type()) {
+  case FilterConfig::KEY_CACHING:
+    return new KeyCachingFilter();
+    break;
+  case FilterConfig::COMPRESSING:
+    return new CompressingFilter();
+    break;
+  case FilterConfig::FIXING_FLOAT:
+    return new FixingFloatFilter();
+    break;
+  case FilterConfig::NOISE:
+    return new AddNoiseFilter();
+  default:
+    CHECK(false) << "unknown filter type";
+  }
+}
+
+FilterConfig *Filter::find(FilterConfig::Type type, mltools::Task *task) {
+  for (int i = 0; i < task->filter_size(); ++i) {
+    if (task->filter(i).type() == type) {
+      return task->mutable_filter(i);
+    }
+  }
+  return nullptr;
+}
+} // namespace mltools
